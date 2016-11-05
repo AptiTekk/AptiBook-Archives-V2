3f7d7df8be9ff91c2d5ae8c2fdaaea94c26efa23
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-11-04 23:51:47 -0600

    Added file copyrights.

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2016 AptiTekk, LLC. (https://AptiTekk.com/) - All Rights Reserved
+ * Unauthorized copying of any part of AptiBook, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {enableProdMode} from "@angular/core";
 import {AppModule} from "./app.module";
