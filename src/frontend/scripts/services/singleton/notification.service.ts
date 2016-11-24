573287b28c95792ee01a4292c17ebb3cc007ace3
Pasha Shestakov <rcvideos12@gmail.com>
2016-11-24 01:04:50 -0700

    Computer merge, bug fixed.

--- src/frontend/scripts/services/singleton/notification.service.ts
+++ src/frontend/scripts/services/singleton/notification.service.ts
@@ -14,14 +14,14 @@ export class NotificationService {
 
     public getNotifications(user: User): Observable<UnreadNotification[]>{
         console.log("method called");
-        return Observable.create(listner =>{
+        return Observable.create(listener =>{
             if(user == undefined)
-                listner.next(undefined);
+                listener.next(undefined);
             else
                 console.log("calling GET")
                 this.apiService.get("notifications/user/" + user.id).subscribe(
-                    response => listner.next(<UnreadNotification[]> response),
-                    err => listner.next(undefined)
+                    response => listener.next(<UnreadNotification[]> response),
+                    err => listener.next(undefined)
                 )
         });
     }
