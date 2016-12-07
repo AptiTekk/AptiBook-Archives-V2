826b771915dcf2896760b3f64b7499dea075f491
Mitch Talmadge <mitch@mitchtalmadge.com>
2016-12-07 16:39:58 -0700

    Added dynamic switch width based on labels.

--- src/frontend/scripts/components/toggle-switch/toggle-switch.component.ts
+++ src/frontend/scripts/components/toggle-switch/toggle-switch.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, forwardRef} from "@angular/core";
+import {Component, Input, forwardRef, AfterViewInit} from "@angular/core";
 import {NG_VALUE_ACCESSOR, ControlValueAccessor} from "@angular/forms";
 
 @Component({
@@ -13,14 +13,29 @@ import {NG_VALUE_ACCESSOR, ControlValueAccessor} from "@angular/forms";
         }
     ]
 })
-export class ToggleSwitchComponent implements ControlValueAccessor {
+export class ToggleSwitchComponent implements AfterViewInit, ControlValueAccessor {
 
     @Input()
     onText: string = "On";
     @Input()
     offText: string = "Off";
 
+    switchWidth: string = '0px';
+
     on: boolean = false;
+
+    ngAfterViewInit(): void {
+        this.calculateSwitchWidth();
+    }
+
+    calculateSwitchWidth() {
+        let maxLabelLength = this.onText.length;
+
+        if (this.offText.length > maxLabelLength)
+            maxLabelLength = this.offText.length;
+
+        this.switchWidth = (maxLabelLength * 13) + 'px';
+    }
 
     toggle() {
         this.on = !this.on;
