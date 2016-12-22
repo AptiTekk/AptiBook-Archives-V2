bb2836e0f2723792b5dfaf2c6507508efb018e01
Pasha Shestakov <rcvideos12@gmail.com>
2016-12-22 15:26:09 -0700

    WIP, bug somewhere.

--- src/frontend/scripts/page-components/secure-page/management/calendar-page/calendar-page.component.ts
+++ src/frontend/scripts/page-components/secure-page/management/calendar-page/calendar-page.component.ts
@@ -3,12 +3,65 @@
  * Unauthorized copying of any part of AptiBook, via any medium, is strictly prohibited.
  * Proprietary and confidential.
  */
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {UserGroupService} from "../../../../services/singleton/usergroup.service";
+import {UserGroup} from "../../../../models/user-group.model";
+import {APIService} from "../../../../services/singleton/api.service";
+import {ResourceCategoryService} from "../../../../services/singleton/resource-category.service";
+import {ReservationInfoModalComponent} from "../../../../components/reservation-info-modal/reservation-info-modal.component";
+import {User} from "../../../../models/user.model";
+import {ResourceCategory} from "../../../../models/resource-category.model";
+import {Reservation} from "../../../../models/reservation.model";
+import {AuthService} from "../../../../services/singleton/auth.service";
 
 @Component({
     selector: 'calendar-page',
     templateUrl: 'calendar-page.component.html'
 })
 export class CalendarPageComponent{
+    userGroupFilter: UserGroup[];
 
+    @ViewChild('reservationInfoModal')
+    reservationInfoModal: ReservationInfoModalComponent;
+
+    makingNewReservation: boolean = false;
+
+    currentUser: User;
+
+    resourceCategories: ResourceCategory[];
+    enabledResourceCategories: ResourceCategory[];
+
+    filterOnlyUsersEvents: boolean = false;
+
+    onCalendarEventClicked(event: Reservation) {
+        this.reservationInfoModal.display(event);
+    }
+
+    onNewReservationStart() {
+        this.makingNewReservation = true;
+    }
+
+    onCancelNewReservation() {
+        this.makingNewReservation = false;
+    }
+
+    updateEnabledResourceCategories() {
+        this.enabledResourceCategories = this.resourceCategories ? this.resourceCategories.filter(category => category['enabled']) : [];
+    }
+
+    constructor(private userGroupService: UserGroupService, private apiService: APIService, private resourceCategoryService: ResourceCategoryService, private authService: AuthService) {
+        authService.getUser().subscribe(user => this.currentUser = user);
+        userGroupService.getUserGroupHierarchyDownFilter();
+        userGroupService.getUserGroupHierarchyDown().subscribe(response => {
+            this.userGroupFilter = response;
+            console.log("Response size: " + this.userGroupFilter.length);
+        });
+
+        this.resourceCategoryService.getResourceCategories().take(1).subscribe(resourceCategory => {
+            this.resourceCategories = resourceCategory.map(category => {
+                category['enabled'] = true;
+                return category;
+            });
+        });
+    }
 }
--- src/frontend/scripts/services/singleton/usergroup.service.ts
+++ src/frontend/scripts/services/singleton/usergroup.service.ts
@@ -2,15 +2,34 @@ import {Injectable} from "@angular/core";
 import {APIService} from "./api.service";
 import {Observable, ReplaySubject} from "rxjs";
 import {UserGroup} from "../../models/user-group.model";
+import {AuthService} from "./auth.service";
+import {User} from "../../models/user.model";
 
 @Injectable()
 export class UserGroupService {
 
     private rootUserGroup: ReplaySubject<UserGroup> = new ReplaySubject<UserGroup>(1);
+    private user: User;
+    private userGroupHierarchyDown: ReplaySubject<UserGroup[]> = new ReplaySubject<UserGroup[]>(1);
 
-    constructor(private apiService: APIService) {
-        this.reloadRootUserGroup();
+    constructor(private apiService: APIService, private authService: AuthService) {
+
     }
+
+
+    public getUserGroupHierarchyDown(): ReplaySubject<UserGroup[]> {
+        return this.userGroupHierarchyDown;
+    }
+
+    public getUserGroupHierarchyDownFilter() {
+        this.authService.getUser().subscribe(user => this.user);
+        if (this.user)
+            this.apiService.get("/userGroups/hierarchyDown/" + this.user.id).subscribe(
+                response => this.userGroupHierarchyDown.next(response),
+                err => this.userGroupHierarchyDown.next([])
+            );
+    }
+
 
     public patchUserGroup(userGroup: UserGroup): Observable<boolean> {
         return Observable.create(listener => {
