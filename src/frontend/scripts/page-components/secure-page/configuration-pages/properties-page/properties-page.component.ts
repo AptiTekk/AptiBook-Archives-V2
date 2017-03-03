18dfc714895b09cfa2fb74c52f2e41003c873ce8
Pasha Shestakov <rcvideos12@gmail.com>
2017-03-02 21:24:28 -0700

    Appears fully functional, need work on view.

--- src/frontend/scripts/page-components/secure-page/configuration-pages/properties-page/properties-page.component.ts
+++ src/frontend/scripts/page-components/secure-page/configuration-pages/properties-page/properties-page.component.ts
@@ -1,9 +1,10 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 import {APIService} from "../../../../services/singleton/api.service";
 import {PropertiesService} from "../../../../services/singleton/properties-service";
 import {Property} from "../../../../models/properties.model";
 import {ReplaySubject} from "rxjs";
 import {FormGroup, FormBuilder} from "@angular/forms";
+import {AlertComponent} from "../../../../components/alert/alert.component";
 
 @Component({
     selector: 'properties-page',
@@ -12,38 +13,47 @@ import {FormGroup, FormBuilder} from "@angular/forms";
 export class PropertiesPageComponent {
 
 
-    //private properties: ReplaySubject<Properties[]> = new ReplaySubject(1);
-    properties: Property[];
+    @ViewChild('changeAlert')
+    changeAlert: AlertComponent;
+
+    public properties: Property[];
     formGroup: FormGroup;
 
     constructor(private propertiesService: PropertiesService, formBuilder: FormBuilder) {
+        propertiesService.getAllProperties().subscribe(properties => {
+            this.properties = properties;
+            this.formGroup = formBuilder.group({
+                PERSONALIZATION_ORGANIZATION_NAME: [this.properties[0].propertyValue],
+                REGISTRATION_ENABLED: [(this.properties[1].propertyValue == 'true')],
+                GOOGLE_SIGN_IN_ENABLED: [(this.properties[2].propertyValue == 'true')],
+                GOOGLE_SIGN_IN_WHITELIST: [this.properties[3].propertyValue],
+                DATE_TIME_TIMEZONE: [this.properties[4].propertyValue]
+            });
 
-        this.formGroup = formBuilder.group({
-            PERSONALIZATION_ORGANIZATION_NAME: [],
-            REGISTRATION_ENABLED: [],
-            GOOGLE_SIGN_IN_ENABLED: [],
-            GOOGLE_SIGN_IN_WHITELIST: [],
-            DATE_TIME_TIMEZONE: []
+
         });
-        propertiesService.getAllProperties().subscribe(properties => this.properties = properties);
+
     }
 
     onSubmit() {
-        //TODO: Make patch methods in properties service, find what field 'dirty' from formGroup control and path values.
-
-        if (this.formGroup.controls['PERSONALIZATION_ORGANIZATION_NAME'].dirty) {
-            //its dirty so patch entity
-            this.properties[0].propertyValue = this.formGroup.controls['PERSONALIZATION_ORGANIZATION_NAME'].value;
-            this.propertiesService.patchProperty(this.properties[0]).subscribe(
-                response => {
-                    //console.log("yay it worked");
-                    //TODO: Make alert and display success
-                },
-                err => {
-                    console.log("broken");
-                }
-            )
+        let i = 0;
+        for(let controlName in this.formGroup.controls)
+        {
+            if (this.formGroup.controls[controlName].dirty) {
+                this.properties[i].propertyValue = this.formGroup.controls[controlName].value;
+                this.propertiesService.patchProperty(this.properties[i]).subscribe(
+                    response => {
+                        this.changeAlert.display("Changes have been saved.");
+                    },
+                    err => {
+                        console.log("broken");
+                    }
+                )
+            }
+            i++;
         }
+
+
 
     }
 
