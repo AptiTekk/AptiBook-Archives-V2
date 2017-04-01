68d290083cd44583484d7ad5c71a103f9abe17b0
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-04-01 16:05:43 -0600

    Cleanup

--- src/frontend/scripts/models/property.model.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2016 AptiTekk, LLC. (https://AptiTekk.com/) - All Rights Reserved
- * Unauthorized copying of any part of AptiBook, via any medium, is strictly prohibited.
- * Proprietary and confidential.
- */
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=property.model.js.map
--- src/frontend/scripts/vendors/hammerjs.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2016 AptiTekk, LLC. (https://AptiTekk.com/) - All Rights Reserved
- * Unauthorized copying of any part of AptiBook, via any medium, is strictly prohibited.
- * Proprietary and confidential.
- */
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("hammerjs");
-//# sourceMappingURL=hammerjs.js.map
